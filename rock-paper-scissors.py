737776e9829c3deccb1423ad49f0910a821ca693
Sogand N <snoroozeydian@gmail.com>
2023-04-14 06:02:58 +0430

    some parts of part 2

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,9 +1,12 @@
 import random
 
+user_name = str(input('Input your name: '))
+
+
 user_score = 0
 system_score = 0
 
-print('User E Mohtaram!')
+print(user_name ,'E Mohtaram!')
 print('please choose a valid answer!')
 print('valid answers are:')
 print('   Rock:  Rock, rock, R & r ...')
@@ -16,17 +19,17 @@ while True:
     
         if user_choice == 'Rock' or user_choice == 'rock' or user_choice == 'R' or user_choice == 'r':
             user_choice = 'Rock'
-            print('Your Choice:', user_choice)
+            print(user_name,'choosed:', user_choice)
             break
 
         elif user_choice == 'Paper' or user_choice == 'paper' or user_choice == 'P' or user_choice == 'p':
             user_choice = 'Paper'
-            print('Your Choice:', user_choice)
+            print(user_name,'choosed:', user_choice)
             break
 
         elif user_choice == 'Scissors' or user_choice == 'scissors' or user_choice == 'S' or user_choice == 's':
             user_choice = 'Scissors'
-            print('Your Choice:', user_choice)
+            print(user_name,'choosed:', user_choice)
             break
 
         else:
@@ -37,11 +40,11 @@ while True:
     print('System Choice:', system_choice)
 
     if user_choice == system_choice:
-        print('Result: Equal !')
+        print('Result: Drow !')
         
     if user_choice == 'Rock':
         if system_choice == 'Scissors':
-            print('Result: You won !')
+            print('Result:', user_name, 'won !')
             user_score = user_score + 1
         if system_choice == 'Paper':
             print('Result: System won !')
@@ -49,7 +52,7 @@ while True:
 
     if user_choice == 'Paper':
         if system_choice == 'Rock':
-            print('Result: You won !')
+            print('Result:', user_name, 'won !')
             user_score = user_score + 1
         if system_choice == 'Scissors':
             print('Result: System won !')
@@ -57,7 +60,7 @@ while True:
 
     if user_choice == 'Scissors':
         if system_choice == 'Paper':
-            print('Result: You won !')
+            print('Result:', user_name, 'won !')
             user_score = user_score + 1
         if system_choice == 'Rock':
             print('Result: System won !')
@@ -66,18 +69,20 @@ while True:
     if user_score == 3 or system_score == 3:
         break
 
-    print('Your score is =', user_score)
+    print(user_name, 'score is =', user_score)
     print('System score is =', system_score)
 
     print('----------------------------')
     
-print('Your score is =', user_score)
+print(user_name, 'score is =', user_score)
 print('System score is =', system_score)
 
 print('----------------------------')
 print('----------------------------')
 
-if user_score == 3:
-    print('Final winner is YOU !')
-if system_score == 3:
-    print('Final winner is SYSTEM !')
+with open('attachment_modules_files/win_score.py', 'r') as file:
+    winer_score = file.read()
+    if user_score == int(winer_score):
+        print('Final winner is YOU !')
+    if system_score == int(winer_score):
+        print('Final winner is SYSTEM !')
